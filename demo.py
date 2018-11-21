1eb02f00f67c41128ccbe25666dee73b9897b0cf
qinfen <qinfen_python@163.com>
2018-11-21 17:18:00 +0800

    django only

--- demo.py
+++
@@ -1,3 +0,0 @@
-print("this --  is my first github file")
-print("this is my first github file")
-print("this ++++  is my first github file")
--- demo1.py
+++
@@ -1,2 +0,0 @@
-print("this is new demo file")
-print("test branch fix the file")
